9d1f23328cab70a0247670e2de02b0739e8d63a6
derogate <afiqf.as@gmail.com>
2021-06-13 09:38:31 +0800

    remove unused props in IndexPg.js

--- src/Pages/IndexPg.js
+++ src/Pages/IndexPg.js
@@ -5,7 +5,7 @@ import Body from "../components/Body";
 // ||| This IndexPg.js is NOT related to frontend src/index.js (purpose: simply code to render App.js).
 // ||| This IndexPg.js is related to frontend src/App.js url path.
 // ||| This IndexPg.js purpose: redirecting to login or dashboard (based on Boolean of localStorage token)
-const IndexPg = (props) => {
+const IndexPg = () => {
   return (
     <div className="container-fluid">
       <Header />
